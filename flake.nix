3e876e3333eefe94decda3179c51a0ee7a74e3f2
diamondburned <diamond@arikawa-hi.me>
2024-01-21 03:03:00 -0800

    Add CI and Flakes package

--- flake.nix
+++ flake.nix
@@ -4,33 +4,62 @@
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
+		gomod2nix.url = "github:nix-community/gomod2nix";
+		gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
+		gomod2nix.inputs.flake-utils.follows = "flake-utils";
 	};
 
-	outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-		with import nixpkgs { inherit system; };
-		{
-			devShell = mkShell {
-				packages = [
-					go
-					gopls
-					go-tools
+	outputs =
+		{ self, nixpkgs, flake-utils, gomod2nix }:
 
-					sqlc
+		flake-utils.lib.eachDefaultSystem (system:
+			with nixpkgs.legacyPackages.${system};
+			with gomod2nix.legacyPackages.${system};
+			{
+				packages.default = buildGoApplication {
+					pname = "february-frenzy";
+					version = "git";
 
-					nodejs
-					dart-sass
+					pwd = ./.;
+					src = ./.;
+					modules = ./gomod2nix.toml;
 
-					python3
-					python3Packages.black
-					pyright
-				];
+					preBuild = "go generate ./...";
+					doCheck = false;
 
-				shellHook = ''
-					python3 -m venv .venv
-					source .venv/bin/activate
-					export PATH="$PATH:$(git rev-parse --show-toplevel)/node_modules/.bin"
-				'';
-			};
-		}
-	);
+					subPackages = [
+						"."
+						"./cmd/competitionctl"
+					];
+
+					nativeBuildInputs = [
+						sqlc
+						dart-sass
+					];
+				};
+				devShell = mkShell {
+					packages = [
+						go
+						gopls
+						go-tools
+						(gomod2nix.legacyPackages.${system}).gomod2nix
+	
+						sqlc
+	
+						nodejs
+						dart-sass
+	
+						python3
+						python3Packages.black
+						pyright
+					];
+	
+					shellHook = ''
+						python3 -m venv .venv
+						source .venv/bin/activate
+						export PATH="$PATH:$(git rev-parse --show-toplevel)/node_modules/.bin"
+					'';
+				};
+			}
+		);
 }
