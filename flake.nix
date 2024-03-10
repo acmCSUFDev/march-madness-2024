044834ed23e46a19b6c947aa0f0cbae019776cf0
diamondburned <diamond@libdb.so>
2024-03-10 01:46:18 -0800

    Get rid of NodeJS from development env

--- flake.nix
+++ flake.nix
@@ -4,13 +4,18 @@
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
+
 		gomod2nix.url = "github:nix-community/gomod2nix";
 		gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
 		gomod2nix.inputs.flake-utils.follows = "flake-utils";
+
+		prettier-gohtml-nix.url = "github:diamondburned/prettier-gohtml-nix";
+		prettier-gohtml-nix.inputs.nixpkgs.follows = "nixpkgs";
+		prettier-gohtml-nix.inputs.flake-utils.follows = "flake-utils";
 	};
 
 	outputs =
-		{ self, nixpkgs, flake-utils, gomod2nix }:
+		{ self, nixpkgs, flake-utils, gomod2nix, prettier-gohtml-nix }:
 
 		flake-utils.lib.eachDefaultSystem (system:
 			with nixpkgs.legacyPackages.${system};
@@ -39,59 +44,26 @@
 				};
 				devShell = mkShell {
 					packages = [
+						# Go Tools.
 						go
 						gopls
 						go-tools
-						(gomod2nix.legacyPackages.${system}).gomod2nix
-	
+						gomod2nix.packages.${system}.default
 						sqlc
 	
-						nodejs
+						# Web/JavaScript Tools.
 						dart-sass
+						prettier-gohtml-nix.packages.${system}.default
 	
+						# Python Tools.
 						python3
 						python3Packages.black
 						pyright
-
-						(writeShellScriptBin "saq-watch" ''
-							set -eo pipefail
-							
-							main() {
-								if ! command -v saq &> /dev/null; then
-									echo "saq could not be found" >&2
-									echo "get it at https://github.com/diamondburned/saq" >&2
-									exit 1
-								fi
-							
-								saq \
-									-s localhost:8391 \
-									-t localhost:8300 \
-									-x ./server/frontend/package-lock.json \
-									-x ./server/frontend/static \
-									-- "$BASH_SOURCE" build_and_run "$@"
-							}
-							
-							build_and_run() {
-								go generate ./...
-								go build -o ''${TMPDIR:-/tmp}/february-frenzy
-								exec ''${TMPDIR:-/tmp}/february-frenzy "$@"
-							}
-							
-							case "$1" in
-								build_and_run)
-									build_and_run "''${@:2}"
-									;;
-								*)
-									main "$@"
-									;;
-							esac
-						'')
 					];
 	
 					shellHook = ''
 						python3 -m venv .venv
 						source .venv/bin/activate
-						export PATH="$PATH:$(git rev-parse --show-toplevel)/node_modules/.bin"
 					'';
 				};
 			}
